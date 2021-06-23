8ebf2eb45fa68e17381763b5f93ccc6910ed0d81
VijayNandakumarkumar <vijay.nandakumar1990@gmail.com>
2021-06-23 23:37:09 +0530

    WhatsApp close: Custom emoji picker implementation.
    * Added customer emoji picker.
    * Handled context switch between emoji picker and multiline keyboard.

--- whatsapp/lib/pages/IndividualChatPage.dart
+++ whatsapp/lib/pages/IndividualChatPage.dart
@@ -1,3 +1,4 @@
+import 'package:emoji_picker/emoji_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/model/ChatModel.dart';
 
@@ -11,6 +12,23 @@ class IndividualChatPage extends StatefulWidget {
 }
 
 class _IndividualChatPageState extends State<IndividualChatPage> {
+  bool shouldShowEmojiPicker = false;
+  FocusNode focusNode = FocusNode();
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        setState(() {
+          shouldShowEmojiPicker = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +47,8 @@ class _IndividualChatPageState extends State<IndividualChatPage> {
               Padding(
                 padding: const EdgeInsets.only(left: 8.0),
                 child: CircleAvatar(
-                  child: Icon(
+                  child:
+                  Icon(
                       widget.chatModel.isGroup ? Icons.group : Icons.person,
                       color: Colors.white),
                   backgroundColor: Colors.grey,
@@ -39,19 +58,22 @@ class _IndividualChatPageState extends State<IndividualChatPage> {
             ],
           ),
         ),
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.chatModel.name,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            Text(
-              "last seen today at " + widget.chatModel.time,
-              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 13),
-            ),
-          ],
+        title: InkWell(
+          onTap: () {},
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.chatModel.name,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Text(
+                "last seen today at " + widget.chatModel.time,
+                style: TextStyle(fontWeight: FontWeight.normal, fontSize: 13),
+              ),
+            ],
+          ),
         ),
         actions: [
           IconButton(
@@ -97,57 +119,100 @@ class _IndividualChatPageState extends State<IndividualChatPage> {
           ),
         ],
       ),
-      body: Stack(
-        children: [
-          ListView(),
-          Container(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width - 55,
-                    child: Card(
-                      margin: EdgeInsets.only(right: 2, left: 2, bottom: 8),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25)),
-                      child: TextFormField(
-                        textAlignVertical: TextAlignVertical.center,
-                        keyboardType: TextInputType.multiline,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            prefixIcon:
-                                IconButton(icon: Icon(Icons.emoji_emotions)),
-                            hintText: "Type a message",
-                            suffixIcon: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(icon: Icon(Icons.attach_file)),
-                                IconButton(
-                                    icon: Icon(
-                                  Icons.camera_alt,
-                                ))
-                              ],
+      body: WillPopScope(
+        child: Stack(
+          children: [
+            ListView(),
+            Container(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: MediaQuery
+                              .of(context)
+                              .size
+                              .width - 55,
+                          child: Card(
+                            margin: EdgeInsets.only(right: 2, left: 2, bottom: 8),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25)),
+                            child: TextFormField(
+                              maxLines: 5,
+                              minLines: 1,
+                              controller: textEditingController,
+                              focusNode: focusNode,
+                              textAlignVertical: TextAlignVertical.center,
+                              keyboardType: TextInputType.multiline,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon:
+                                  IconButton(icon: Icon(Icons.emoji_emotions), onPressed: () {
+                                    // textEditingController.text = "podaPanni";
+                                    focusNode.unfocus();
+                                    focusNode.canRequestFocus = false;
+                                    setState(() {
+                                      shouldShowEmojiPicker = !shouldShowEmojiPicker;
+                                    });
+                                  },),
+                                  hintText: "Type a message",
+                                  suffixIcon: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(icon: Icon(Icons.attach_file)),
+                                      IconButton(
+                                          icon: Icon(
+                                            Icons.camera_alt,
+                                          ))
+                                    ],
+                                  ),
+                                  contentPadding: EdgeInsets.all(5)),
                             ),
-                            contentPadding: EdgeInsets.all(5)),
-                      ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                          const EdgeInsets.only(bottom: 8, right: 2, left: 2),
+                          child: CircleAvatar(
+                            radius: 25,
+                            backgroundColor: Colors.green,
+                            child: IconButton(
+                              icon: Icon(Icons.mic, color: Colors.white,),
+                              onPressed: () {},),
+                          ),
+                        )
+                      ],
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(bottom: 8, right: 2, left: 2),
-                    child: CircleAvatar(
-                      radius: 25,
-                      backgroundColor: Colors.green,
-                      child: IconButton(icon: Icon(Icons.mic, color: Colors.white,), onPressed: () {},),
-                    ),
-                  )
-                ],
+                    shouldShowEmojiPicker ? emojiPicker() : Container(),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
+        onWillPop: () {
+          if (shouldShowEmojiPicker) {
+            setState(() {
+              shouldShowEmojiPicker = false;
+            });
+          } else {
+            Navigator.pop(context);
+          }
+          return Future.value(false);
+        },
       ),
     );
+  }
+
+  Widget emojiPicker() {
+    return EmojiPicker(
+        rows: 4,
+        columns: 7,
+        onEmojiSelected: (emoji, category) {
+          textEditingController.text = textEditingController.text + emoji.emoji;
+        });
   }
 }
